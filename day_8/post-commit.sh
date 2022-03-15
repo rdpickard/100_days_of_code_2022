916f82c42447c7aa64962e8a0bd05cccf2a6e27c
Robert Daniel Pickard <robert.daniel.pickard@chalkfarm.org>
2022-03-15 15:37:14 -0400

    day_8 pre-commit streak image create script

--- day_8/post-commit.sh
+++ day_8/post-commit.sh
@@ -9,7 +9,7 @@ then
   exit
 fi
 
-diff= exec git diff --cached --name-only --diff-filter=ACM
-
+diff_v= exec git diff --cached --name-only --diff-filter=ACM
+echo $diff_v
 git add $PWD/media/streak.jpg
 git commit --amend --no-verify $PWD/media/streak.jpg
