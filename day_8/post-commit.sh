99d08870d45b9ca8edee2cf8d0c90454e3a1121a
Robert Daniel Pickard <robert.daniel.pickard@chalkfarm.org>
2022-03-15 15:27:45 -0400

    day_8 pre-commit streak image create script

---
+++ day_8/post-commit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Creating commit streak image"
+PYTHON_ENV=$(python -c "import sys; sys.stdout.write('1') if hasattr(sys, 'real_prefix') or (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix) else sys.stdout.write('0')")
+
+if [ $PYTHON_ENV -eq 0 ]
+then
+  echo "Not in virtual env. Not going to try to create streak image"
+  exit
+fi
+
+git add $PWD/media/streak.jpg
+git commit --amend --no-verify $PWD/media/streak.jpg
