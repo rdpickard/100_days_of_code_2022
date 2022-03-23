98b4a959cb2c72efc8e003913a09815669f50566
Robert Daniel Pickard <robert.daniel.pickard@chalkfarm.org>
2022-03-23 17:42:24 -0400

    day_16 Go web frameworks

--- day_16/gin_sample.go
+++ day_16/gin_sample.go
@@ -5,13 +5,22 @@ import (
 	"net/http"
 )
 
-func APIAddImage(c *gin.Context) {
-	var books []string
-	c.JSON(http.StatusOK, gin.H{"data": books})
+func APIImage(c *gin.Context) {
+	var images []string
+
+	if c.Request.Method == "GET" {
+		c.JSON(http.StatusOK, gin.H{"images": images})
+	} else if c.Request.Method == "POST" {
+		c.JSON(http.StatusOK, gin.H{"image": "derp"})
+	} else {
+		c.JSON(http.StatusMethodNotAllowed, gin.H{"image": "crap"})
+	}
+
 }
 
-func APILabelImage(c *gin.Context) {
+func APIGetImage(c *gin.Context) {
 	var books []string
+	print(c)
 	c.JSON(http.StatusOK, gin.H{"data": books})
 }
 
@@ -21,6 +30,10 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
 	})
+
+	r.PUT("/api/image", APIImage)
+	r.GET("/api/image", APIImage)
+	r.POST("/api/image", APIImage)
 
 	r.Run()
 }
