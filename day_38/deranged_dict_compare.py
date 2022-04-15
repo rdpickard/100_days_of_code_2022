d551f7a9b5076cc3cd11e31a5326e7895c3098d7
rdpickard <code@chalkfarm.mx>
2022-04-14 20:58:19 -0400

    day_38 i made it more deranged

--- day_38/deranged_dict_compare.py
+++ day_38/deranged_dict_compare.py
@@ -6,12 +6,13 @@ not_subset_dict_one = {"one": "fred"}
 not_subset_dict_two = {"one": 1, "two": 2, "deep": "fred"}
 not_subset_dict_three = {"one": 1, "two": 2, "deep": {"d1deep": {"d2one": "fred"}}}
 
-deranged_compare_dicts = lambda source_dict, compare_item: type(compare_item[1]) is dict and all(list(map(lambda nested_dict_value: deranged_compare_dicts(source_dict[compare_item[0]], nested_dict_value), compare_item[1].items()))) or source_dict.get(compare_item[0],None) == compare_item[1]
+deranged_compare_dicts = lambda mass_dict, subset_dict: all(list(map(lambda compare_item: type(compare_item[1]) is dict and deranged_compare_dicts(mass_dict[compare_item[0]], compare_item[1]) or mass_dict.get(compare_item[0], None) == compare_item[1] ,subset_dict.items())))
 
-print("Should be TRUE -> {}".format(all(list(map(lambda compare_item: deranged_compare_dicts(large_dict_one, compare_item), subset_dict_one.items())))))
-print("Should be TRUE -> {}".format(all(list(map(lambda compare_item: deranged_compare_dicts(large_dict_one, compare_item), subset_dict_two.items())))))
-print("Should be TRUE -> {}".format(all(list(map(lambda compare_item: deranged_compare_dicts(large_dict_one, compare_item), subset_dict_three.items())))))
+print("Should be TRUE -> {}".format(deranged_compare_dicts(large_dict_one, subset_dict_one)))
+print("Should be TRUE -> {}".format(deranged_compare_dicts(large_dict_one, subset_dict_two)))
+print("Should be TRUE -> {}".format(deranged_compare_dicts(large_dict_one, subset_dict_three)))
 
-print("Should be FALSE -> {}".format(all(list(map(lambda compare_item: deranged_compare_dicts(large_dict_one, compare_item), not_subset_dict_one.items())))))
-print("Should be FALSE -> {}".format(all(list(map(lambda compare_item: deranged_compare_dicts(large_dict_one, compare_item), not_subset_dict_two.items())))))
-print("Should be FALSE -> {}".format(all(list(map(lambda compare_item: deranged_compare_dicts(large_dict_one, compare_item), not_subset_dict_three.items())))))
+print("Should be FALSE -> {}".format(deranged_compare_dicts(large_dict_one, not_subset_dict_one)))
+print("Should be FALSE -> {}".format(deranged_compare_dicts(large_dict_one, not_subset_dict_two)))
+print("Should be FALSE -> {}".format(deranged_compare_dicts(large_dict_one, not_subset_dict_three)))
+
