5228acf638303885f7a60c197a7436058fa51dd5
rdpickard <code@chalkfarm.mx>
2022-04-15 20:21:43 -0400

    day_39

--- day_38/deranged_dict_compare.py
+++ day_38/deranged_dict_compare.py
@@ -6,7 +6,7 @@ not_subset_dict_one = {"one": "fred"}
 not_subset_dict_two = {"one": 1, "two": 2, "deep": "fred"}
 not_subset_dict_three = {"one": 1, "two": 2, "deep": {"d1deep": {"d2one": "fred"}}}
 
-deranged_compare_dicts = lambda mass_dict, subset_dict: all(list(map(lambda compare_item: type(compare_item[1]) is dict and deranged_compare_dicts(mass_dict[compare_item[0]], compare_item[1]) or mass_dict.get(compare_item[0], None) == compare_item[1] ,subset_dict.items())))
+deranged_compare_dicts = lambda mass_dict, subset_dict: all(list(map(lambda compare_item: type(compare_item[1]) is dict and deranged_compare_dicts(mass_dict[compare_item[0]], compare_item[1]) or mass_dict.get(compare_item[0], None) == compare_item[1], subset_dict.items())))
 
 print("Should be TRUE -> {}".format(deranged_compare_dicts(large_dict_one, subset_dict_one)))
 print("Should be TRUE -> {}".format(deranged_compare_dicts(large_dict_one, subset_dict_two)))
